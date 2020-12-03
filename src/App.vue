6fc96f29ea5235437bf1c5fb6f8880c12185d183
RenektonChr <1054346513@qq.com>
2020-12-03 23:27:23 +0800

    renekton list header

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,72 @@
 <template>
   <div class="container">
-    <ColumnList :list="list" />
+    <global-header :user="currentUser" />
+    <column-list :list="list" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import ColumnList, { ColumnProps } from "./components/ColumnList/index.vue";
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList/index.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader/index.vue'
+const currentUser: UserProps = {
+  isLogin: true,
+  name: '崔浩然',
+  id: 1
+}
 const testData: ColumnProps[] = [
   {
     id: 1,
-    title: "test1的专栏",
-    description: "这是test1的专栏，有一段非常有意思的简介，可以更新一下哦",
-    avatar:
-      "https://image.baidu.com/search/detail?ct=503316480&z=0&tn=baiduimagedetail&ipn=d&word=%E5%A4%B4%E5%83%8F&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=-1&hd=undefined&latest=undefined&copyright=undefined&cs=3515088285,869864046&os=3235646919,3450947003&simid=3404717260,436834222&pn=0&rn=1&di=196790&ln=30&fr=&fmq=1461834053046_R&fm=&ic=0&s=0&se=&sme=&tab=0&width=&height=&face=undefined&is=0,0&istype=2&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&objurl=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201411%2F29%2F20141129194517_5Z2Lu.png&rpstart=0&rpnum=0&adpicid=0&force=undefined",
+    title: 'test1的专栏',
+    description: '这是test1的专栏，有一段非常有意思的简介，可以更新一下哦这是test1的专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607009800487&di=aaf8828d94e4b4d6374e42ee53ba656e&imgtype=0&src=http%3A%2F%2Fdiy.qqjay.com%2Fu%2Ffiles%2F2012%2F0217%2Fb693a3b6d232ffe861da22287c888729.jpg'
   },
   {
     id: 2,
-    title: "test2的专栏",
-    description: "这是test2的专栏，有一段非常有意思的简介，可以更新一下哦",
-    avatar:
-      "https://image.baidu.com/search/detail?ct=503316480&z=0&tn=baiduimagedetail&ipn=d&word=%E5%A4%B4%E5%83%8F&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=-1&hd=undefined&latest=undefined&copyright=undefined&cs=3065335715,4197701299&os=2881999140,2042603032&simid=4139949256,547552291&pn=1&rn=1&di=115170&ln=30&fr=&fmq=1461834053046_R&fm=&ic=0&s=0&se=&sme=&tab=0&width=&height=&face=undefined&is=0,0&istype=2&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&objurl=http%3A%2F%2Fdiy.qqjay.com%2Fu%2Ffiles%2F2012%2F0217%2Fb693a3b6d232ffe861da22287c888729.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined",
+    title: 'test2的专栏',
+    description: '这是test2的专栏，有一段非常有意思的简介，可以更新一下哦'
+    // avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607009800487&di=aaf8828d94e4b4d6374e42ee53ba656e&imgtype=0&src=http%3A%2F%2Fdiy.qqjay.com%2Fu%2Ffiles%2F2012%2F0217%2Fb693a3b6d232ffe861da22287c888729.jpg'
   },
-];
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是test3的专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607009800487&di=aaf8828d94e4b4d6374e42ee53ba656e&imgtype=0&src=http%3A%2F%2Fdiy.qqjay.com%2Fu%2Ffiles%2F2012%2F0217%2Fb693a3b6d232ffe861da22287c888729.jpg'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是test4的专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: ''
+  },
+  {
+    id: 5,
+    title: 'test5的专栏',
+    description: '这是test5的专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607009800487&di=aaf8828d94e4b4d6374e42ee53ba656e&imgtype=0&src=http%3A%2F%2Fdiy.qqjay.com%2Fu%2Ffiles%2F2012%2F0217%2Fb693a3b6d232ffe861da22287c888729.jpg'
+  },
+  {
+    id: 6,
+    title: 'test6的专栏',
+    description: '这是test6的专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607009800487&di=aaf8828d94e4b4d6374e42ee53ba656e&imgtype=0&src=http%3A%2F%2Fdiy.qqjay.com%2Fu%2Ffiles%2F2012%2F0217%2Fb693a3b6d232ffe861da22287c888729.jpg'
+  }
+]
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
     ColumnList,
+    GlobalHeader
   },
-  setup() {
+  setup () {
     return {
       list: testData,
-    };
-  },
-});
+      currentUser
+    }
+  }
+})
 </script>
 
 <style>
--- src/components/ColumnList/index.vue
+++ src/components/ColumnList/index.vue
@@ -1,33 +1,50 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title" />
-      <h5>{{ column.title }}</h5>
-      <p>{{ column.description }}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in columnlist" :key="column.id" class="col-4 mb-3">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="column.avatar" class="rounded-circle border border-light w-25 my-4" alt="..." />
+          <h5 class="card-title">{{column.title}}</h5>
+          <p class="card-text text-left">
+            {{ column.description }}
+          </p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from 'vue'
 
 export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string;
   description: string;
 }
 export default defineComponent({
-  name: "ColumnList",
+  name: 'ColumnList',
   props: {
     list: {
       type: Array as PropType<ColumnProps[]>,
-      required: true,
-    },
+      required: true
+    }
   },
-  setup() {},
-});
+  setup (props) {
+    const columnlist = computed(() => {
+      return props.list.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/avator.jpg')
+        }
+        return column
+      })
+    })
+
+    return { columnlist }
+  }
+})
 </script>
 
 <style>
