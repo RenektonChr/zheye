39454322f42909f400ff1f35ce365c0ec10b167f
cuihaoran <1054346513@qq.com>
2020-12-04 18:57:12 +0800

    cuihaoran

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,29 @@
   <div class="container">
     <global-header :user="currentUser" />
     <column-list :list="list" />
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        >
+        <div class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList/index.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader/index.vue'
@@ -52,17 +70,34 @@ const testData: ColumnProps[] = [
     avatar: require('../src/assets/pic.jpg')
   }
 ]
+const eamilReg = /^(([^()[\]\\.,;:\s@"]+(\.[^()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
-    GlobalHeader
+    GlobalHeader,
+    ColumnList
   },
   setup () {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can not be empty'
+      } else if (!eamilReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid email'
+      }
+    }
     return {
       list: testData,
-      currentUser
+      currentUser,
+      emailRef,
+      validateEmail
     }
   }
 })
