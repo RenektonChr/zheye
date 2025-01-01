94798de7007e182c545e7cb0c4291476832c8f74
cuihaoran <1054346513@qq.com>
2025-01-01 15:39:26 +0800

    feat: 测试

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="container w-100">
     <global-header :user="currentUser" @handle-logout="handleLogout" />
-    <loading v-if="isLoading" text="拼命加载中" background="rgba(0,0,0,.8)"></loading>
+    <loading
+      v-if="isLoading"
+      text="拼命加载中"
+      background="rgba(0,0,0,.8)"
+    ></loading>
     <router-view />
     <Footer class="text-center py-4 text-seconddary bg-light mt-6">
       <small>
         <ul class="list-inline mb-0">
           <li class="list-inline-item">格物</li>
           <li class="list-inline-item">致知</li>
-          <li class="list-inline-item">诚意</li>
-          <li class="list-inline-item">正心</li>
-          <li class="list-inline-item">@ 2020 然哥专栏</li>
         </ul>
       </small>
     </Footer>
@@ -18,51 +19,53 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, watch } from 'vue'
-import { useStore } from 'vuex'
-import { useRoute, useRouter } from 'vue-router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import GlobalHeader from './components/GlobalHeader/index.vue'
-import Footer from './components/Footer/index.vue'
-import Loading from './components/Loading/index.vue'
-import createMessage from './components/CreateMessage/index'
-import { GlobalDataProps } from './store/index'
+import { defineComponent, computed, watch } from "vue";
+import { useStore } from "vuex";
+import { useRoute, useRouter } from "vue-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import GlobalHeader from "./components/GlobalHeader/index.vue";
+import Footer from "./components/Footer/index.vue";
+import Loading from "./components/Loading/index.vue";
+import createMessage from "./components/CreateMessage/index";
+import { GlobalDataProps } from "./store/index";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     GlobalHeader,
     Footer,
-    Loading
+    Loading,
   },
-  setup () {
-    const store = useStore<GlobalDataProps>()
-    const router = useRouter()
-    const route = useRoute()
-    const currentUser = computed(() => store.state.user)
-    const isLoading = computed(() => store.state.loading)
-    const error = computed(() => store.state.error)
-    watch(() => error.value.status, () => {
-      const { status, message } = error.value
-      if (status && message) {
-        createMessage(message, 'error')
+  setup() {
+    const store = useStore<GlobalDataProps>();
+    const router = useRouter();
+    const route = useRoute();
+    const currentUser = computed(() => store.state.user);
+    const isLoading = computed(() => store.state.loading);
+    const error = computed(() => store.state.error);
+    watch(
+      () => error.value.status,
+      () => {
+        const { status, message } = error.value;
+        if (status && message) {
+          createMessage(message, "error");
+        }
       }
-    })
+    );
     const handleLogout = () => {
-      store.commit('logout')
-      if (route.fullPath !== '/') {
-        router.push('/')
+      store.commit("logout");
+      if (route.fullPath !== "/") {
+        router.push("/");
       }
-    }
+    };
     return {
       currentUser,
       isLoading,
       error,
-      handleLogout
-    }
-  }
-})
+      handleLogout,
+    };
+  },
+});
 </script>
 
-<style>
-</style>
+<style></style>
