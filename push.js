e982cc127bf38eaa679b20d32246c5b5d2ad4994
cuihaoran <1054346513@qq.com>
2025-01-01 17:56:24 +0800

    feat: xxx

---
+++ push.js
@@ -0,0 +1,207 @@
+const event = "push";
+const payload = {
+  ref: "refs/heads/master",
+  before: "0245996e2d44ddc6113144e96fa63e6d562334f8",
+  after: "ea0a4c915a5c3b44f00cc70f6ed9bd163905aba7",
+  repository: {
+    id: 317715329,
+    node_id: "MDEwOlJlcG9zaXRvcnkzMTc3MTUzMjk=",
+    name: "zheye",
+    full_name: "RenektonChr/zheye",
+    private: false,
+    owner: {
+      name: "RenektonChr",
+      email: "1054346513@qq.com",
+      login: "RenektonChr",
+      id: 30065304,
+      node_id: "MDQ6VXNlcjMwMDY1MzA0",
+      avatar_url: "https://avatars.githubusercontent.com/u/30065304?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/RenektonChr",
+      html_url: "https://github.com/RenektonChr",
+      followers_url: "https://api.github.com/users/RenektonChr/followers",
+      following_url:
+        "https://api.github.com/users/RenektonChr/following{/other_user}",
+      gists_url: "https://api.github.com/users/RenektonChr/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/RenektonChr/starred{/owner}{/repo}",
+      subscriptions_url:
+        "https://api.github.com/users/RenektonChr/subscriptions",
+      organizations_url: "https://api.github.com/users/RenektonChr/orgs",
+      repos_url: "https://api.github.com/users/RenektonChr/repos",
+      events_url: "https://api.github.com/users/RenektonChr/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/RenektonChr/received_events",
+      type: "User",
+      user_view_type: "public",
+      site_admin: false,
+    },
+    html_url: "https://github.com/RenektonChr/zheye",
+    description: "仿知乎专栏",
+    fork: false,
+    url: "https://github.com/RenektonChr/zheye",
+    forks_url: "https://api.github.com/repos/RenektonChr/zheye/forks",
+    keys_url: "https://api.github.com/repos/RenektonChr/zheye/keys{/key_id}",
+    collaborators_url:
+      "https://api.github.com/repos/RenektonChr/zheye/collaborators{/collaborator}",
+    teams_url: "https://api.github.com/repos/RenektonChr/zheye/teams",
+    hooks_url: "https://api.github.com/repos/RenektonChr/zheye/hooks",
+    issue_events_url:
+      "https://api.github.com/repos/RenektonChr/zheye/issues/events{/number}",
+    events_url: "https://api.github.com/repos/RenektonChr/zheye/events",
+    assignees_url:
+      "https://api.github.com/repos/RenektonChr/zheye/assignees{/user}",
+    branches_url:
+      "https://api.github.com/repos/RenektonChr/zheye/branches{/branch}",
+    tags_url: "https://api.github.com/repos/RenektonChr/zheye/tags",
+    blobs_url: "https://api.github.com/repos/RenektonChr/zheye/git/blobs{/sha}",
+    git_tags_url:
+      "https://api.github.com/repos/RenektonChr/zheye/git/tags{/sha}",
+    git_refs_url:
+      "https://api.github.com/repos/RenektonChr/zheye/git/refs{/sha}",
+    trees_url: "https://api.github.com/repos/RenektonChr/zheye/git/trees{/sha}",
+    statuses_url:
+      "https://api.github.com/repos/RenektonChr/zheye/statuses/{sha}",
+    languages_url: "https://api.github.com/repos/RenektonChr/zheye/languages",
+    stargazers_url: "https://api.github.com/repos/RenektonChr/zheye/stargazers",
+    contributors_url:
+      "https://api.github.com/repos/RenektonChr/zheye/contributors",
+    subscribers_url:
+      "https://api.github.com/repos/RenektonChr/zheye/subscribers",
+    subscription_url:
+      "https://api.github.com/repos/RenektonChr/zheye/subscription",
+    commits_url: "https://api.github.com/repos/RenektonChr/zheye/commits{/sha}",
+    git_commits_url:
+      "https://api.github.com/repos/RenektonChr/zheye/git/commits{/sha}",
+    comments_url:
+      "https://api.github.com/repos/RenektonChr/zheye/comments{/number}",
+    issue_comment_url:
+      "https://api.github.com/repos/RenektonChr/zheye/issues/comments{/number}",
+    contents_url:
+      "https://api.github.com/repos/RenektonChr/zheye/contents/{+path}",
+    compare_url:
+      "https://api.github.com/repos/RenektonChr/zheye/compare/{base}...{head}",
+    merges_url: "https://api.github.com/repos/RenektonChr/zheye/merges",
+    archive_url:
+      "https://api.github.com/repos/RenektonChr/zheye/{archive_format}{/ref}",
+    downloads_url: "https://api.github.com/repos/RenektonChr/zheye/downloads",
+    issues_url:
+      "https://api.github.com/repos/RenektonChr/zheye/issues{/number}",
+    pulls_url: "https://api.github.com/repos/RenektonChr/zheye/pulls{/number}",
+    milestones_url:
+      "https://api.github.com/repos/RenektonChr/zheye/milestones{/number}",
+    notifications_url:
+      "https://api.github.com/repos/RenektonChr/zheye/notifications{?since,all,participating}",
+    labels_url: "https://api.github.com/repos/RenektonChr/zheye/labels{/name}",
+    releases_url:
+      "https://api.github.com/repos/RenektonChr/zheye/releases{/id}",
+    deployments_url:
+      "https://api.github.com/repos/RenektonChr/zheye/deployments",
+    created_at: 1606872661,
+    updated_at: "2025-01-01T09:00:07Z",
+    pushed_at: 1735722650,
+    git_url: "git://github.com/RenektonChr/zheye.git",
+    ssh_url: "git@github.com:RenektonChr/zheye.git",
+    clone_url: "https://github.com/RenektonChr/zheye.git",
+    svn_url: "https://github.com/RenektonChr/zheye",
+    homepage: null,
+    size: 950,
+    stargazers_count: 6,
+    watchers_count: 6,
+    language: "Vue",
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    has_discussions: false,
+    forks_count: 0,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 0,
+    license: null,
+    allow_forking: true,
+    is_template: false,
+    web_commit_signoff_required: false,
+    topics: [],
+    visibility: "public",
+    forks: 0,
+    open_issues: 0,
+    watchers: 6,
+    default_branch: "master",
+    stargazers: 6,
+    master_branch: "master",
+  },
+  pusher: { name: "RenektonChr", email: "1054346513@qq.com" },
+  sender: {
+    login: "RenektonChr",
+    id: 30065304,
+    node_id: "MDQ6VXNlcjMwMDY1MzA0",
+    avatar_url: "https://avatars.githubusercontent.com/u/30065304?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/RenektonChr",
+    html_url: "https://github.com/RenektonChr",
+    followers_url: "https://api.github.com/users/RenektonChr/followers",
+    following_url:
+      "https://api.github.com/users/RenektonChr/following{/other_user}",
+    gists_url: "https://api.github.com/users/RenektonChr/gists{/gist_id}",
+    starred_url:
+      "https://api.github.com/users/RenektonChr/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/RenektonChr/subscriptions",
+    organizations_url: "https://api.github.com/users/RenektonChr/orgs",
+    repos_url: "https://api.github.com/users/RenektonChr/repos",
+    events_url: "https://api.github.com/users/RenektonChr/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/RenektonChr/received_events",
+    type: "User",
+    user_view_type: "public",
+    site_admin: false,
+  },
+  installation: {
+    id: 59032648,
+    node_id: "MDIzOkludGVncmF0aW9uSW5zdGFsbGF0aW9uNTkwMzI2NDg=",
+  },
+  created: false,
+  deleted: false,
+  forced: false,
+  base_ref: null,
+  compare:
+    "https://github.com/RenektonChr/zheye/compare/0245996e2d44...ea0a4c915a5c",
+  commits: [
+    {
+      id: "ea0a4c915a5c3b44f00cc70f6ed9bd163905aba7",
+      tree_id: "57dca0095f57d86f876a0278d72ce669f3c02979",
+      distinct: true,
+      message: "feat: xxx",
+      timestamp: "2025-01-01T17:10:35+08:00",
+      url: "https://github.com/RenektonChr/zheye/commit/ea0a4c915a5c3b44f00cc70f6ed9bd163905aba7",
+      author: [Object],
+      committer: [Object],
+      added: [],
+      removed: [],
+      modified: [Array],
+    },
+  ],
+  head_commit: {
+    id: "ea0a4c915a5c3b44f00cc70f6ed9bd163905aba7",
+    tree_id: "57dca0095f57d86f876a0278d72ce669f3c02979",
+    distinct: true,
+    message: "feat: xxx",
+    timestamp: "2025-01-01T17:10:35+08:00",
+    url: "https://github.com/RenektonChr/zheye/commit/ea0a4c915a5c3b44f00cc70f6ed9bd163905aba7",
+    author: {
+      name: "cuihaoran",
+      email: "1054346513@qq.com",
+      username: "RenektonChr",
+    },
+    committer: {
+      name: "cuihaoran",
+      email: "1054346513@qq.com",
+      username: "RenektonChr",
+    },
+    added: [],
+    removed: [],
+    modified: ["src/App.vue"],
+  },
+};
